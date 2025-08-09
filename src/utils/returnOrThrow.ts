cd2348947560ad7cb0e48a19db4264a671ddcd80
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-08-10 00:34:16 +0100

    feat: add returnOrThrow utility function for error handling

---
+++ src/utils/returnOrThrow.ts
@@ -0,0 +1,28 @@
+export type TReturnOrThrow<T> = {
+    shouldThrow: boolean;
+    throwable: Error;
+    returns?: T;
+}
+
+
+/**
+ * @typedef {Object} ReturnOrThrow<T>
+ * @property {boolean} shouldThrow - Whether to throw an error or not
+ * @property {Error} throwable - The error to throw
+ * @property {T} [returns] - The value to return
+ */
+
+/**
+ * Returns a value or throws an error
+ * @param {TReturnOrThrow<T>} param0 - The options to return or throw
+ * @returns {T} The value to return
+ */
+export const returnOrThrow = <T>({ shouldThrow, throwable, returns }: TReturnOrThrow<T>): T => {
+    if (shouldThrow && throwable) {
+        throw throwable;
+    }
+    if (returns !== undefined) {
+        return returns;
+    }
+    throw new Error('Invalid properties');
+};
