b74e2545bacfe66b40a5c8fca680ee44eaa25b0e
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-08-09 21:23:22 +0100

    feat: integrate check-exports command in lefthook and enhance package.json scripts

--- src/casts/index.ts
+++ src/casts/index.ts
@@ -1,7 +1,5 @@
 export * from "./BaseCastable";
 export * from "./Castable";
-export * from './CastException';
-export * from './castObject';
+export * from "./CastException";
+export * from "./castObject";
 export * from "./HasCastableConcern";
-export * from "./types.t";
-
