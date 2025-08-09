8484a1a635115281c2b3e2fcc4ffe9e77809047f
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-08-09 20:51:32 +0100

    feat: add utils module export to index for enhanced functionality

---
+++ src/casts/castObject.ts
@@ -0,0 +1,8 @@
+import { BaseCastable } from "./BaseCastable"
+import { TCasts } from "./types.t"
+
+const castObject = <ReturnType = unknown>(data: unknown, casts: TCasts): ReturnType => {
+    return new BaseCastable().getCastFromObject<ReturnType>(data as Record<string, unknown>, casts)
+}
+
+export default castObject
